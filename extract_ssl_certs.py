6ed7941bbda84d2df157ddc2d6f10a688c5194be
Master-13 <382967242@qq.com>
2016-11-01 09:15:03 +0800

    更新20161101
    更新了extract_ssl_cert.py，针对Muiltiple Handshake类型做了修改。

--- extract_ssl_certs.py
+++ extract_ssl_certs.py
@@ -3,8 +3,6 @@
 import dpkt
 import struct, socket, sys, os, argparse, logging, md5
 
-import multiprocessing
-
 parser = argparse.ArgumentParser(description='Extract SSL certs')
 parser.add_argument("-f", "--file", action='store', help="Extract SSL certificates from a single file.", default = None)
 parser.add_argument("-d", "--dir", action='store', help="Extract SSL certificates from a directory containing cap files.", default = None)
@@ -55,7 +53,7 @@ if args.exclude != None:
         pass
 
 def extract_file(filepath):
-    if not fn.endswith('cap'):
+    if not filepath.endswith('cap'):
         return
     
     global certcount
@@ -98,7 +96,7 @@ def extract_file(filepath):
     except Exception,e:
         print e.message
     f.close()
-    
+        
     #A->B和B->A是按两个流统计的，所以遍历一边源，就可以遍历到所有情况。
     for t4,dic in tcp_piece.iteritems():    #根据4元组进行组流
         srcip=t4[0]
@@ -120,42 +118,46 @@ def extract_file(filepath):
         while(curpos<totallen):
             #如果特别小，直接跳过
             if totallen-curpos<12: break
-            #如果不是传递证书的包，跳过
-            if sslcombined[curpos]!='\x16' or sslcombined[curpos+5]!='\x0b':
-                curpos+=5+struct.unpack('!H', sslcombined[curpos+3:curpos+5])[0]
-                continue
-            #取得总长度
-            certlen=struct.unpack('!I', '\x00'+sslcombined[curpos+9:curpos+12])[0]
-            if certlen>totallen:    #证书的长度超过了数据包的长度，通常是数据包数据丢失导致的
+            #如果不是Handshake类型
+            if sslcombined[curpos]!='\x16':
                 break
-            if certlen>50000:
-                log.warn('Very big cert.')
-                curpos+=5+struct.unpack('!H', sslcombined[curpos+3:curpos+5])[0]
-                continue
+            handshake_len=struct.unpack('!H', sslcombined[curpos+3:curpos+5])[0]
+            curpos+=curpos+5
+            cur_handshakelen=0
+            while(cur_handshakelen<handshake_len):
+                this_handshake_len=struct.unpack('!I', '\x00'+sslcombined[curpos+1:curpos+4])[0]
+                if sslcombined[curpos]=='\x0b': #如果这一段是证书
+                    certlen=struct.unpack('!I', '\x00'+sslcombined[curpos+4:curpos+7])[0]
+                    if certlen>totallen:    #证书的长度超过了数据包的长度，通常是数据包数据丢失导致的
+                        break                    
+                    curpos+=7
+                    sub_cert_len=0  #所有子证书的总大小            
+                    sub_cert_count=1    #子证书编号，编号形成证书链，越靠下越小
+                    while(sub_cert_len<certlen):
+                        this_sub_len=struct.unpack('!I', '\x00'+sslcombined[curpos:curpos+3])[0]   #当前子证书大小
+                        curpos+=3
+                        this_sub_cert=sslcombined[curpos:curpos+this_sub_len]
+                        sub_cert_len+=this_sub_len+3    #+3是“证书长度”，3个字节
+                        curpos+=this_sub_len
+                        md5cert=md5.md5(this_sub_cert).hexdigest()
+                        filename='%s_%d_%d_%s.cer' % (srcip, sport, sub_cert_count, md5cert)
+                        with open('certs\\%s' % filename, 'wb') as f:
+                            f.write(this_sub_cert)
+                        log.info(filename)
+                        sub_cert_count+=1         
+                    certcount+=sub_cert_count-1
+                else:
+                    curpos+=this_handshake_len+4  #不是证书直接跳过
+                cur_handshakelen+=this_handshake_len+4
             
-            curpos+=12
-            sub_cert_len=0  #所有子证书的总大小            
-            sub_cert_count=1    #子证书编号，编号形成证书链，越靠下越小
-            while(sub_cert_len<certlen):
-                this_sub_len=struct.unpack('!I', '\x00'+sslcombined[curpos:curpos+3])[0]   #当前子证书大小
-                curpos+=3
-                this_sub_cert=sslcombined[curpos:curpos+this_sub_len]
-                sub_cert_len+=this_sub_len+3    #+3是“证书长度”，3个字节
-                curpos+=this_sub_len
-                md5cert=md5.md5(this_sub_cert).hexdigest()
-                filename='%s_%d_%d_%s.cer' % (srcip, sport, sub_cert_count, md5cert)
-                with open('certs\\%s' % filename, 'wb') as f:
-                    f.write(this_sub_cert)
-                log.info(filename)
-                sub_cert_count+=1         
-            certcount+=sub_cert_count-1
-            break
+            if cur_handshakelen>=handshake_len:
+                continue
+                
 
 if args.dir!=None:
-    p=multiprocessing.Pool(4)   #254有4个核，所以选了4
     for root, parent, files in os.walk(args.dir):
-        if not files==[]:
-            p.map(extract_file, map(lambda x:root+os.sep+x, files))
+        for f in files:
+            extract_file(root+os.sep+f)
             
 elif args.file!=None:
     extract_file(args.file)
